4d2ec7142ff64102750396dde8a328eb28553aa9
NavozZ <navodyatheshan4@gmail.com>
2025-04-14 13:03:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# visit_sri_lanka_travel_guide_app
+ PUSL2021 Computing Group Project (24/AY/AU/M)
---
+++ android/app/src/main/kotlin/com/example/visit_sri_lanka_travel_guide_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.visit_sri_lanka_travel_guide_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
