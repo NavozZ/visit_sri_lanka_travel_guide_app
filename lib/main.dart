c1a2c1c11e37881a5ad322bb12502cce5bd836e9
NavozZ <warunikanchana321@gmail.com>
2025-04-22 20:53:11 +0530

    Opening Screen Done
    Added Splash Screen
    added on boarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:visit_sri_lanka_travel_guide_app/Homepage.dart';
 import 'package:visit_sri_lanka_travel_guide_app/Providers/Tours_provider.dart';
 import 'package:visit_sri_lanka_travel_guide_app/Providers/places_provider.dart';
+import 'package:visit_sri_lanka_travel_guide_app/Screens/Authetication%20Screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,13 +28,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Homepage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Vist Sri Lanka',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snap) {
+              if (snap.data == null) {
+                return const AuthScreen();
+              } else {
+                return const Homepage();
+              }
+            }));
   }
 }
